98d994f5e18d8fd092f79644c1e4adfbe9812a60
DanielAntic05 <9698108@itisrossi.vi.it>
2024-04-21 15:55:08 +0200

    modified

---
+++ shanghai/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class ShanghaiGame
+{
+	private const int N_STICKS = 10;
+	private static int[] graph = new int[N_STICKS + 1];
+
+
+    static bool IsWinningPosition(int sticksLeft)
+    {
+        if (sticksLeft == 0)
+            return false;
+
+        // Controls if the current player can force the opponent into a losing position
+        for (int i = 1; i <= 3; i++)
+            if (sticksLeft - i >= 0 && graph[sticksLeft - i] == 0)
+                return true;
+
+        return false;
+    }
+
+
+    static void Main(string[] args)
+    {
+        for (int sticksLeft = 1; sticksLeft <= N_STICKS; sticksLeft++)
+            for (int i = 1; i <= 3; i++)
+                if (sticksLeft - i >= 0 && graph[sticksLeft - i] == 0)
+                {
+                    graph[sticksLeft] = 1;
+                    break;
+                }
+
+        if (IsWinningPosition(N_STICKS))
+            Console.WriteLine("Player 1 wins!");
+        else
+            Console.WriteLine("Player 2 wins!");
+    }
+}
